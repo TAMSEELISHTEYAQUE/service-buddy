7209d9ce89ed2ad9ffeb199fcc7948ed1d669f00
Tamseel Ishteyaque <tamseelishteyaque62@gmail.com>
2026-04-05 21:10:18 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,66 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
 
+    if(user === "admin" && pass === "1234"){
+        localStorage.setItem("user", user);
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid login");
+    }
+}
+const services = [
+    {name: "Electrician", desc: "Fix wiring, switches", price: "₹200"},
+    {name: "Plumber", desc: "Pipe & leakage repair", price: "₹150"},
+    {name: "AC Repair", desc: "Cooling issue fix", price: "₹300"},
+    {name: "Cleaner", desc: "Home cleaning", price: "₹100"}
+];
+
+function loadServices(){
+    let container = document.getElementById("services");
+    container.innerHTML = "";
+
+    services.forEach(service => {
+        container.innerHTML += `
+        <div class="card">
+            <h3>${service.name}</h3>
+            <p>${service.desc}</p>
+            <p><b>${service.price}</b></p>
+            <button onclick="bookService('${service.name}')">Book</button>
+        </div>
+        `;
+    });
+}
+
+function searchService(){
+    let query = document.getElementById("search").value.toLowerCase();
+
+    let filtered = services.filter(s => 
+        s.name.toLowerCase().includes(query)
+    );
+
+    let container = document.getElementById("services");
+    container.innerHTML = "";
+
+    filtered.forEach(service => {
+        container.innerHTML += `
+        <div class="card">
+            <h3>${service.name}</h3>
+            <p>${service.desc}</p>
+            <p><b>${service.price}</b></p>
+            <button onclick="bookService('${service.name}')">Book</button>
+        </div>
+        `;
+    });
+}
+
+function bookService(name){
+    alert("Service Booked: " + name);
+}
+
+function logout(){
+    localStorage.removeItem("user");
+    window.location.href = "login.html";
+}
+
+window.onload = loadServices;
